e95db3049a3dfe90f54543de484d4c4e9e69cbb6
Frederic Walch <github@frederic.tech>
2023-09-14 17:36:47 +0200

    Add db tasks

---
+++ src/task/db/schema_load.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+namespace :db do
+  namespace :schema do
+    desc "Database schema load"
+    task load: :environment do
+      input_path = File.join("db", "schema.sql")
+
+      command = []
+      command << ["PGPASSWORD=\"#{ENV.fetch("DB_PASSWORD", nil)}\"", ] if ENV.fetch("DB_PASSWORD", nil)
+      command << ["psql"]
+      command << ["-h", ENV.fetch("DB_HOST", nil)] if ENV.fetch("DB_HOST", nil)
+      command << [ENV.fetch("DB_NAME"), "<", input_path]
+      command << [">", "/dev/null"]
+
+      system(command.join(" "), exception: true)
+
+      Dir.glob(File.join("db", "updates", "*.rb")) do |schema_update_file|
+        Application::DB[:schema_migrations].insert(filename: schema_update_file.split("/").last)
+      end
+
+      Application::Logger.info("Schema loaded from #{input_path}.")
+    end
+  end
+end
