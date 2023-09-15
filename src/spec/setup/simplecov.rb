6da5309ea98509b0f329621bd2ccdef0dbc8a44b
Frederic Walch <github@frederic.tech>
2023-09-15 20:11:18 +0200

    Configure simplecov correctly

--- src/spec/setup/simplecov.rb
+++ src/spec/setup/simplecov.rb
@@ -3,5 +3,10 @@
 require "simplecov"
 
 SimpleCov.start do
+  add_group("Error", File.join("app", "error"))
+  add_group("Data", File.join("app", "data"))
+  add_group("Command", File.join("app", "command"))
+  add_group("Validator", File.join("app", "validator"))
+
   add_filter("spec")
 end
